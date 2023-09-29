d4ad8093b66e65e2c0ff6801c59dbc8d241659b5
fenix <fenixtheidragon@gmail.com>
2023-09-29 12:00:32 +0300

    algorithms

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -11,7 +11,7 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
-
+        return new String[]{"winter", "spring", "summer", "autumn"};
     }
 
     /**
@@ -25,7 +25,11 @@ public class ArrayTasks {
      * length = 5  -> [1, 2, 3, 4, 5]
      */
     public int[] generateNumbers(int length) {
-
+        int[] arr = new int[length];
+        for (int a = 0; a < arr.length; a++) {
+            arr[a] = a + 1;
+        }
+        return arr;
     }
 
     /**
@@ -37,7 +41,11 @@ public class ArrayTasks {
      * arr = [5, -3, -4] -> sum = -2
      */
     public int totalSum(int[] arr) {
-
+        int sum = 0;
+        for (int a = 0; a < arr.length; a++) {
+            sum += arr[a];
+        }
+        return sum;
     }
 
     /**
@@ -50,7 +58,12 @@ public class ArrayTasks {
      * arr = [5, -3, -4],   number = 10    ->  -1
      */
     public int findIndexOfNumber(int[] arr, int number) {
-
+        for (int a = 0; a < arr.length; a++) {
+            if (arr[a] == number) {
+                return a;
+            }
+        }
+        return -1;
     }
 
     /**
@@ -63,7 +76,11 @@ public class ArrayTasks {
      * arr = ["pineapple", "apple", "pen"] -> ["pen", "apple", "pineapple"]
      */
     public String[] reverseArray(String[] arr) {
-
+        String[] reverseArray = new String[arr.length];
+        for (int a = 0; a < arr.length; a++) {
+            reverseArray[a] = arr[arr.length - 1 - a];
+        }
+        return reverseArray;
     }
 
     /**
@@ -78,12 +95,26 @@ public class ArrayTasks {
      * arr = [1, 2]         -> [1, 2]
      */
     public int[] getOnlyPositiveNumbers(int[] arr) {
-
+        int length = 0;
+        for (int a = 0; a < arr.length; a++) {
+            if (arr[a] > 0) {
+                length++;
+            }
+        }
+        int[] na = new int[length];
+        int counter = 0;
+        for (int a = 0; a < arr.length; a++) {
+            if (arr[a] > 0) {
+                na[counter] = arr[a];
+                counter++;
+            }
+        }
+        return na;
     }
 
     /**
      * Return a sorted, ragged, two-dimensional int[][] array following these rules:
-     * Incoming one-dimensional arrays must be arranged in ascending order of their length;
+     * IncoindexOfMinimumg one-dimensional arrays must be arranged in ascending order of their length;
      * numbers in all one-dimensional arrays must be in ascending order.
      * <p>
      * Example:
@@ -92,6 +123,48 @@ public class ArrayTasks {
      * arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
     public int[][] sortRaggedArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("{");
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + ";");
+            }
+            System.out.print("},\n");
+        }
+        System.out.println("--------------------");
 
+        int lastIndex = arr.length;
+        int[][] sorted = arr.clone();
+        int currentIndex = 0;
+        while (currentIndex < lastIndex) {
+            for (int a = currentIndex; a < lastIndex; a++) {
+                if (sorted[currentIndex].length > sorted[a].length) {
+                    int[][] tempSorted = sorted.clone();
+                    sorted[currentIndex] = sorted[a].clone();
+                    System.arraycopy(tempSorted, currentIndex, sorted, currentIndex + 1, a - currentIndex);
+                }
+                int[] temp = sorted[currentIndex].clone();
+                int tempCounter = 0;
+                while (tempCounter < temp.length) {
+                    for (int b = tempCounter; b < temp.length; b++) {
+                        if (temp[tempCounter] > temp[b]) {
+                            int c = temp[tempCounter];
+                            temp[tempCounter] = temp[b];
+                            temp[b] = c;
+                        }
+                    }
+                    tempCounter++;
+                }
+                sorted[currentIndex] = temp.clone();
+            }
+            currentIndex++;
+        }
+        for (int i = 0; i < sorted.length; i++) {
+            System.out.print("{");
+            for (int j = 0; j < sorted[i].length; j++) {
+                System.out.print(sorted[i][j] + ";");
+            }
+            System.out.print("},\n");
+        }
+        return sorted;
     }
 }
